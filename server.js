fd04f80778dd29f8e92f021cbd3fd4521c5209fd
leesujeong <leesujeong@ubisam.com>
2024-02-13 15:23:35 +0900

    서버연결 체크 추가

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-    origin : 'http://localhost:3000',
-    methods : ['GET', 'POST', 'DELETE', 'PUT'],
-    credentials : true,
-})
+        origin : 'http://localhost:3000',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        credentials : true,
+    })
 );
 
 app.post('/login', login);
@@ -29,6 +29,13 @@ app.get('/accesstoken', accessToken);
 app.get('/refreshtoken', refreshToken);
 app.get('/login/success', loginSucess);
 app.post('/logout', logout);
+
+app.get('/api/data', (req, res) => {
+    res.json({
+        message: 'This is data from the server',
+        items: ['Item 1', 'Item 2', 'Item 3'] //나중에 필요하면 사용 현재는 서버 연결 테스트 용
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server is on ${process.env.PORT}`);
